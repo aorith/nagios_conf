2f5b7ccfdac29a153074dd0af2c4a0c76a9265cf
Manuel Sanchez Pinar <aomanu@gmail.com>
2020-08-03 20:31:09 +0200

    added reboot check after updates

---
+++ libexec/reboot_required.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+[ -z "$1" ] && exit 2
+
+if /usr/bin/ssh "$1" "test -f /var/run/reboot-required"; then
+    echo "Reinicio necesario"
+    exit 1
+fi
+
+echo "No hace falta reiniciar"
+
