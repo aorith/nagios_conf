5f2ea8e8cd9b59104a6a9ece343e9f33e2d2b00f
Manuel Sanchez Pinar <aomanu@gmail.com>
2020-09-24 08:35:24 +0200

    io_stats

--- libexec/io_stats.sh
+++ libexec/io_stats.sh
@@ -4,17 +4,15 @@
 
 SERVER=$1
 
-last_entry_date="$(ssh "aorith@${SERVER}" "sadf -dt -- -b |cut -d';' -f3|tail -1")"
-output="$(ssh "aorith@${SERVER}" "sadf -dt -- -b |grep \"${last_entry_date}\"")"
+output="$(ssh "aorith@${SERVER}" "sar -b 1 3 |grep 'Average:'")"
 [[ -z "$output" ]] && { echo "No data retrieved"; exit 3; }
 
-# hostname;interval;timestamp;tps;rtps;wtps;bread/s;bwrtn/s
-# 0        1        2         3   4    5    6       7
-# pve;60;2020-09-15 00:01:01;72.65;39.63;33.03;2160.71;820.66
+# 08:29:53 AM       tps      rtps      wtps   bread/s   bwrtn/s
+# 08:29:56 AM      0.00      0.00      0.00      0.00      0.00
+# Average:         0.00      0.00      0.00      0.00      0.00
+#  0                1         2         3         4         5
 
 status=0
-old_IFS=$IFS
-IFS=';'
 count=0
 ARR=()
 for field in $output;
@@ -22,21 +20,18 @@ do
     ARR[$count]="$field"
     count=$(( count + 1 ))
 done
-IFS=$old_IFS
 
 re='[0-9\.]+'
-if [[ ! ${ARR[4]} =~ $re ]]; then
-    # exit if first field is an '#' that means we have no data after rotate
-    [[ ${ARR[0]} == "#" ]] && exit 0
+if [[ ! ${ARR[1]} =~ $re ]]; then
     echo "Error reading sadf(sar) output."
     exit 3
 fi
 
-tps=${ARR[3]}
-rtps=${ARR[4]}
-wtps=${ARR[5]}
-bread_s=$(echo "${ARR[6]} * 512" |bc)
-bwrtn_s=$(echo "${ARR[7]} * 512" |bc)
+tps=${ARR[1]}
+rtps=${ARR[2]}
+wtps=${ARR[3]}
+bread_s=$(echo "${ARR[4]} * 512" |bc)
+bwrtn_s=$(echo "${ARR[5]} * 512" |bc)
 
 text="$tps tps - $rtps rtps - $wtps wtps - $bread_s bread/s - $bwrtn_s bwrtn/s"
 perfdata="tps=${tps};;;0; rtps=${rtps};;;0; wtps=${wtps};;;0; bread_s=${bread_s};;;0; bwrtn_s=${bwrtn_s};;;0;"
