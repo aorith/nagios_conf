e00e2e14b45f1c1e5ffe078e9883a99a4947416d
Manuel Sanchez Pinar <aomanu@gmail.com>
2020-09-24 08:59:45 +0200

    iface_stats

--- libexec/iface_stats.sh
+++ libexec/iface_stats.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
+LC_ALL=C
 
 [ $# -ne 2 ] && { echo "Bad invocation: \"$0 $*\""; exit 3; }
 
 SERVER=$1
 IFACE=$2
 
-last_entry_date="$(ssh "aorith@${SERVER}" "sadf -dt -- -n DEV --iface=$IFACE |cut -d';' -f3|tail -1")"
-output="$(ssh "aorith@${SERVER}" "sadf -dt -- -n DEV --iface=$IFACE |grep \"${last_entry_date}\"")"
+output="$(ssh "aorith@${SERVER}" "LC_ALL=C sar -n DEV --iface=$IFACE 1 3 |grep 'Average:' |grep -v '%ifutil'")"
 [[ -z "$output" ]] && { echo "No data retrieved"; exit 3; }
 
 ## hostname;interval;timestamp;IFACE;rxpck/s;txpck/s;rxkB/s;txkB/s;rxcmp/s;txcmp/s;rxmcst/s;%ifutil
 #  0        1        2         3     4       5       6      7      8       9       10       11
 #pve;120;2020-09-14 08:28:01;enp6s0f0;38.71;20.18;36.08;2.08;0.00;0.00;0.31;0.03
 
+# Average:        IFACE   rxpck/s   txpck/s    rxkB/s    txkB/s   rxcmp/s   txcmp/s  rxmcst/s   %ifutil
+# Average:         eth0      5.33      5.33      0.27      0.78      0.00      0.00      0.00      0.00
+#  0               1          2         3         4        5          6        7         8         9
+
 status=0
-old_IFS=$IFS
-IFS=';'
 count=0
 ARR=()
 for field in $output;
@@ -23,18 +25,15 @@ do
     ARR[$count]="$field"
     count=$(( count + 1 ))
 done
-IFS=$old_IFS
 
 re='[0-9\.]+'
-if [[ ! ${ARR[4]} =~ $re ]]; then
-    # exit if first field is an '#' that means we have no data after rotate
-    [[ ${ARR[0]} == "#" ]] && exit 0
+if [[ ! ${ARR[2]} =~ $re ]]; then
     echo "Error reading sadf(sar) output."
     exit 3
 fi
 
-text="${ARR[3]} rxKB/s ${ARR[6]} txKB/s ${ARR[7]} Usage ${ARR[11]}"
-perfdata="rxKBs_s=${ARR[6]}KB;;;0; txKBs_s=${ARR[7]}KB;;;0; Usage_perc=${ARR[11]}%;;;0;100"
+text="$IFACE rxKB/s:${ARR[4]} txKB/s:${ARR[5]} Usage:${ARR[9]}"
+perfdata="rxKBs_s=${ARR[4]}KB;;;0; txKBs_s=${ARR[5]}KB;;;0; Usage_perc=${ARR[9]}%;;;0;100"
 
 echo -n "$text"
 echo -n "|"
