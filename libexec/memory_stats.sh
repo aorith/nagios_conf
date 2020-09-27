5f4bf7af5f26a8daf6b9d39f55e656159f5a10ce
Manuel Sanchez Pinar <aomanu@gmail.com>
2020-09-27 11:09:55 +0200

    memstat for lxc

--- libexec/memory_stats.sh
+++ libexec/memory_stats.sh
@@ -5,6 +5,13 @@ LC_ALL=C
 SERVER=$1
 WARN=$2
 CRIT=$3
+WARN_C=$WARN
+CRIT_C=$CRIT
+
+if ssh "aorith@${SERVER}" "sudo grep -qa container=lxc /proc/1/environ"; then
+    WARN_C=$(echo "scale=2; $WARN * 100" |bc -l)
+    CRIT_C=$(echo "scale=2; $CRIT * 100" |bc -l)
+fi
 
 output="$(ssh "aorith@${SERVER}" "LC_ALL=C sar -r ALL 1 3 |grep 'Average:'")"
 [[ -z "$output" ]] && { echo "No data retrieved"; exit 3; }
@@ -39,17 +46,17 @@ kbcommit=${ARR[7]}
 commit_perc=${ARR[8]}
 
 text="MemUsed:${memused_perc}% - Commited:${commit_perc}%"
-perfdata="kbmemfree=${kbmemfree}KB;;;0; kbavail=${kbavail}KB;;;0; kbmemused=${kbmemused}KB;;;0; memused_perc=${memused_perc}%;$WARN;$CRIT;0; kbbuffers=${kbbuffers}KB;;;0; kbcached=${kbcached}KB;;;0; kbcommit=${kbcommit}KB;;;0; commit_perc=${commit_perc}%;$WARN;$CRIT;0;"
+perfdata="kbmemfree=${kbmemfree}KB;;;0; kbavail=${kbavail}KB;;;0; kbmemused=${kbmemused}KB;;;0; memused_perc=${memused_perc}%;$WARN;$CRIT;0; kbbuffers=${kbbuffers}KB;;;0; kbcached=${kbcached}KB;;;0; kbcommit=${kbcommit}KB;;;0; commit_perc=${commit_perc}%;$WARN_C;$CRIT_C;0;"
 
 # testing if $1 > $2
 _decimal_compare() {
     [ ${1%.*} -eq ${2%.*} ] && [ ${1#*.} \> ${2#*.} ] || [ ${1%.*} -gt ${2%.*} ]
 }
 
-if _decimal_compare $memused_perc $CRIT || _decimal_compare $commit_perc $CRIT; then 
+if _decimal_compare $memused_perc $CRIT || _decimal_compare $commit_perc $CRIT_C; then 
     status=2
     text="CRITICAL: ${text}"
-elif _decimal_compare $memused_perc $WARN || _decimal_compare $commit_perc $WARN; then 
+elif _decimal_compare $memused_perc $WARN || _decimal_compare $commit_perc $WARN_C; then 
     status=1
     text="WARNING: ${text}"
 fi
