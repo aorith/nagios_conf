2fe43af0dd58bf0ae566f24f63d333376c9da0e8
Manuel Sanchez Pinar <aomanu@gmail.com>
2020-10-02 19:07:06 +0200

    rclone check

---
+++ libexec/rclone_check.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+status=0
+SERVER=$1
+
+REMOTES="gdrive-msp gdrive-aoasir gdrive-farfe dropbox-ao"
+
+#	"total": 6710886400,
+#	"used": 2295739614,
+#	"free": 4415146786
+for remote in $REMOTES; do
+    output="$(ssh "aorith@${SERVER}" "sudo -u syncthing /home/syncthing/githome/st-backup/rclone/rclone about ${remote}: --json")"
+    total="$(jq '.total' <<< "$output")"
+    used="$(jq '.used' <<< "$output")"
+    free="$(jq '.free' <<< "$output")"
+    text="${text}[$remote total:$total used:$used free:$free] "
+    perfdata="${perfdata} ${remote}_total=${total}b;;;0; ${remote}_used=${used}b;;;0; ${remote}_free=${free}b;;;0; "
+done
+
+echo -n "$text"
+echo -n "|"
+echo "$perfdata"
+exit $status
