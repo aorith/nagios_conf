ff90dc583e465ab07cad060a8a0ef515f5a56ce3
Manuel Sanchez Pinar <aomanu@gmail.com>
2020-11-07 13:22:11 +0100

    qload warn crit

--- libexec/qload_stats.sh
+++ libexec/qload_stats.sh
@@ -39,8 +39,8 @@ perc_load1=$(echo "scale=2; (${load1}*100)/${nprocs}" |bc -l)
 WARN=$(echo "scale=3;0.5 * $nprocs" |bc -l)
 CRIT=$(echo "scale=3;0.7 * $nprocs" |bc -l)
 
-echo "$perc_load1 > 70" && status=1
-echo "$perc_load1 > 90" && status=2
+echo "$perc_load1 > 70" | bc && status=1
+echo "$perc_load1 > 90" | bc && status=2
 
 text="load1(%) ${perc_load1}% - load(1/5/15)m ${load1}/${load5}/${load15} - RunQueueLength $run_queue_length - Tasks $tasks - Blocked $blocked"
 perfdata="load1=${load1};$WARN;$CRIT;0; load5=${load5};$WARN;$CRIT;0; load15=${load15};$WARN;$CRIT;0; run_queue_length=${run_queue_length};10;30;0; tasks=${tasks};;;0; blocked=${blocked};1;5;0; perc_load1=${perc_load1}%;50;80;0;"
